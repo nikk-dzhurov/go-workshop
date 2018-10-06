c8e5c27a17db80be05a2ac1090523cff576a60e8
Nikki Dzhurov <nikkd.phoenix@gmail.com>
2018-10-06 11:30:14 +0300

    Add gorilla/mux router and example default handler

--- cmd/go-workshop/main.go
+++ cmd/go-workshop/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	err := http.ListenAndServe(":8080", nil)
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloHandler)
+
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatalln("Server error: %s\n", err.Error())
 	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, http.StatusText(http.StatusOK))
 }
